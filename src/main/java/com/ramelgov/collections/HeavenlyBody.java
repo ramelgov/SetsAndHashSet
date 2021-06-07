089dd29648e17087e424d3bbedc2633359e72f37
ramelgov <ramelgov1998@gmail.com>
2021-06-07 23:03:11 +0300

    HeavenlyBody class was added.

--- src/main/java/com/ramelgov/collections/HeavenlyBody.java
+++ src/main/java/com/ramelgov/collections/HeavenlyBody.java
@@ -1,4 +1,30 @@
 package com.ramelgov.collections;
 
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ *
+ */
 public class HeavenlyBody {
+    private final String name;
+    private final double orbitalPeriod;
+    private final Set<HeavenlyBody> satellites;
+    // Constructor
+    public HeavenlyBody(String name, double orbitalPeriod, Set<HeavenlyBody> satellites) {
+        this.name = name;
+        this.orbitalPeriod = orbitalPeriod;
+        this.satellites = new HashSet<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getOrbitalPeriod() {
+        return orbitalPeriod;
+    }
+    public  boolean addMoon(HeavenlyBody moon) {
+        return this.satellites.add(moon);
+    }
 }
